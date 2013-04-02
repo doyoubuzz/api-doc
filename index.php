e5b38ca406ab46957949c91e647a9a70ddc9d2e3
Jérémie Pottier <jeremie.pottier@gmail.com>
2013-04-02 12:57:22 +0200

    routing

--- index.php
+++ index.php
@@ -3,30 +3,25 @@
 error_reporting(-1);
 require_once 'libraries/twig/lib/Twig/Autoloader.php';
 
-
 Twig_Autoloader::register();
 
-
 $loader = new Twig_Loader_Filesystem('views');
-
-
 
 
  $twig = new Twig_Environment($loader, array(
   //'cache' => 'cache',
 ));
 
+$env_url = getenv('DOC_URL');
+$path = (!empty($env_url)) ? $env_url : 'http://api-doc.local/';
+$template = str_replace($path, '', 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 
-
-$path = trim($_SERVER['REQUEST_URI'],'/');
-
-if(!$path )
-{
-	$path = 'home';
+if (!$template) {
+    $template = 'home';
 }
 
-echo $twig->render($path.'.twig', array(
-	'template' => $path,
+echo $twig->render($template.'.twig', array(
+	'template' => $template,
 ));
 
 
